7c37905ce6268016589f556136fd42f5261381d6
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-12-30 00:59:03 +0200

    Update SideBar.tsx

--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -1,26 +1,26 @@
-'use client'
+"use client";
 
-import React,{ useState } from 'react';
-import CalendarSideBar from './CalendarSideBar';
-import Create from './create';
-import UserSearch from './UserSearch';
-import { cn } from '@/lib/utils';
-import MyCalendars from './MyCalendars';
+import React, { useState } from "react";
+import CalendarSideBar from "./CalendarSideBar";
+import Create from "./Create";
+import UserSearch from "./UserSearch";
+import { cn } from "@/lib/utils";
+import MyCalendars from "./MyCalendars";
+// import Create from './create';
 
 export default function SideBar() {
-	const [isSideBarOpen] = useState(false);
-	return (
+  const [isSideBarOpen] = useState(false);
+  return (
     <aside
-      className=
-      {cn(
+      className={cn(
         "w-92 hidden border-t px-2 py-3 transition-all duration-300 ease-in-out lg:block",
         !isSideBarOpen && "lg:hidden",
-			)}
-		>
-			<Create />
-			<CalendarSideBar />
-			<UserSearch />
-			<MyCalendars/>
+      )}
+    >
+      <Create />
+      <CalendarSideBar />
+      <UserSearch />
+      <MyCalendars />
     </aside>
   );
 }
