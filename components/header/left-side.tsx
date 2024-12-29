b84433bb55396d07a6b4a4949cba19fb183b7fc8
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-12-29 17:31:32 +0200

    Adding shadcn button and adjusting left-sidebar

--- components/header/left-side.tsx
+++ components/header/left-side.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Button } from '../ui/button'
 
 export default function HeaderLeft() {
-	return <div>left-side</div>;
+	return (
+	<div className='flex items-center gap-3'>
+			<div className="hidden items-center lg:flex">
+				<Button>
+					
+				</Button>
+</div>
+		</div>
+	)
 }
