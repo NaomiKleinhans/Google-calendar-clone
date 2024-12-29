85406717efd7f300dd5550af143cc5cc64d94192
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-12-29 17:16:39 +0200

    Header component added and prettify plugin added for tailwindcss classes sorting

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import HeaderLeft from './left-side';
+import HeaderRight from './right-side';
 
 export default function Header() {
 	return (
-		<div>Header</div>
+		<div className='mx-3 flex items-center justify-between py-4'>
+<HeaderLeft/>
+<HeaderRight/>
+		</div>
 	)
 }
