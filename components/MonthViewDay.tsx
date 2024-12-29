2780d90dc334c8ff33ab1627865310842f5bb3e0
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-12-30 00:54:20 +0200

    Update MonthViewDay.tsx

--- components/MonthViewDay.tsx
+++ components/MonthViewDay.tsx
@@ -9,12 +9,15 @@ export default function MonthViewDay({
   day: dayjs.Dayjs | null;
   rowIndex: number;
 }) {
-  console.log(day);
+  // console.log(day);
   if (!day) {
     return (
       <div className="h-12 w-full border md:h-28 md:w-full lg:h-full"></div>
     );
   }
+  const isFirstDayOfMonth = day.date() === 1;
+
+  const isToday = day.format("DD-MM-YY") === dayjs().format("DD-MM-YY");
 
   return (
     <div
@@ -29,7 +32,15 @@ export default function MonthViewDay({
             {day.format("ddd").toUpperCase()}
           </h4>
         )}
-        <h4 className={cn("text-center text-sm")}>{day.format('D')}</h4>
+        <h4
+          className={cn(
+            "text-center text-sm",
+            isToday &&
+              "flex h-8 w-8 items-center justify-center rounded-full bg-blue-500",
+          )}
+        >
+          {isFirstDayOfMonth ? day.format("MMM D") : day.format("D")}
+        </h4>
       </div>
     </div>
   );
