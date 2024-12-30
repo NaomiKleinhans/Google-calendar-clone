f78da51c64d6ebc8317615913c81fe259469740f
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-12-30 15:07:29 +0200

    calendar view updates

--- components/MainView.tsx
+++ components/MainView.tsx
@@ -1,13 +1,22 @@
+'use client'
 import React from "react";
 import MonthView from "./MonthView";
 import SideBar from "./sidebar/SideBar";
+import { useViewStore } from "@/lib/store";
+import WeekView from "./WeekView";
+import DayView from "./DayView";
 
 export default function MainView() {
+
+  const { selectedView } = useViewStore
+  
   return (
     <div className="flex-2">
       <SideBar/>
 			<div className=""></div>
-			<MonthView/>
+      {selectedView === 'month' && <MonthView />}
+      {selectedView === 'week' && <WeekView />}
+      {selectedView === 'day' && <DayView />}
     </div>
   );
 }
--- components/header/LeftSide.tsx
+++ components/header/LeftSide.tsx
@@ -1,35 +1,110 @@
+'use client'
 import React from "react";
-import { Button } from "../ui/button";
 import { Menu } from "lucide-react";
+import { Button } from "../ui/button";
 import Image from "next/image";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { useDateStore, useToggleSideBarStore, useViewStore } from "@/lib/store";
+import dayjs from "dayjs";
 
 export default function HeaderLeft() {
+  const todaysDate = dayjs();
+  const { userSelectedDate, setDate, setMonth, selectedMonthIndex } =
+    useDateStore();
+
+  const { setSideBarOpen } = useToggleSideBarStore();
+
+  const { selectedView } = useViewStore();
+
+  const handleTodayClick = () => {
+    switch (selectedView) {
+      case "month":
+        setMonth(dayjs().month());
+        break;
+      case "week":
+        setDate(todaysDate);
+        break;
+      case "day":
+        setDate(todaysDate);
+        setMonth(dayjs().month());
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handlePrevClick = () => {
+    switch (selectedView) {
+      case "month":
+        setMonth(selectedMonthIndex - 1);
+        break;
+      case "week":
+        setDate(userSelectedDate.subtract(1, "week"));
+        break;
+      case "day":
+        setDate(userSelectedDate.subtract(1, "day"));
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleNextClick = () => {
+    switch (selectedView) {
+      case "month":
+        setMonth(selectedMonthIndex + 1);
+        break;
+      case "week":
+        setDate(userSelectedDate.add(1, "week"));
+        break;
+      case "day":
+        setDate(userSelectedDate.add(1, "day"));
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <div className="flex items-center gap-3">
       {/*Burger Menu Button*/}
-      <div className="items-center flex">
-        <Button variant="ghost" className="cursor-pointer rounded-full p-2">
+      <div className="flex items-center">
+        <Button
+          onClick={() => setSideBarOpen()}
+          variant="ghost"
+          className="cursor-pointer rounded-full p-2"
+        >
           <Menu className="size-6" />
         </Button>
-				{/*Calendar Icon*/}
+        {/*Calendar Icon*/}
         <Image
-					src="/calendar images/calendar_1_2x.png"
+          src={`/calendar_images/calendar_${todaysDate.date().toString()}_2x.png`}
           width={40}
           height={40}
-          alt="google-calendar-icon"
+          alt="google calendar icon"
         />
         <h1 className="text-xl">Calendar</h1>
       </div>
       {/*Button to go to current day*/}
-      <Button variant="outline">Today</Button>
+      <Button variant="outline" onClick={handleTodayClick}>
+        Today
+      </Button>
       {/*Navigation*/}
       <div className="flex items-center gap-3">
-        <MdKeyboardArrowLeft className="size-6 cursor-pointer font-bold" />
-        <MdKeyboardArrowRight className="size-6 cursor-pointer font-bold" />
+        <MdKeyboardArrowLeft
+          onClick={handlePrevClick}
+          className="size-6 cursor-pointer font-bold"
+        />
+        <MdKeyboardArrowRight
+          onClick={handleNextClick}
+          className="size-6 cursor-pointer font-bold"
+        />
       </div>
       {/*Month and Year currently*/}
-      <h1 className="hidden text-xl lg:block">October 1 2024</h1>
+      <h1 className="hidden text-xl lg:block">
+        {dayjs(new Date(dayjs().year(), selectedMonthIndex)).format(
+          "MMMM YYYY",
+        )}
+      </h1>
     </div>
   );
 }
