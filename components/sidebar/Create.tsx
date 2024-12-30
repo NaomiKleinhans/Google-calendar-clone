c669295c2e242c4eb7011a7fbf570aa1e6c86c2e
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-12-30 15:13:20 +0200

    Sidebar additions

--- components/sidebar/Create.tsx
+++ components/sidebar/Create.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
-import { Button } from '../ui/button';
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
+import { useDateStore } from "@/lib/store";
+import { useCallback, useState } from "react";
+import { SvgIcons } from "../svg-icons";
+import EventPopover from "../event-popover";
 
 export default function Create() {
-	return (
-		<Button></Button>
-	)
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const handleOpenPopover = useCallback((e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsPopoverOpen(true);
+  }, []);
+
+  const handleClosePopover = useCallback(() => {
+    setIsPopoverOpen(false);
+  }, []);
+
+  const { userSelectedDate } = useDateStore();
+
+  return (
+    <>
+      <Button
+        variant="ghost"
+        className="w-[150px] justify-start rounded-full py-6 shadow"
+        onClick={handleOpenPopover}
+      >
+        <SvgIcons.googleCreate className="mr-2 h-8 w-8" /> <span> Create </span>{" "}
+        <ChevronDown />
+      </Button>
+      {isPopoverOpen && (
+        <EventPopover
+          isOpen={isPopoverOpen}
+          onClose={handleClosePopover}
+          date={userSelectedDate.format("YYYY-MM-DD")}
+        />
+      )}
+    </>
+  );
 }
